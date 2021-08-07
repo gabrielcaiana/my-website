a17c4b11570b1cfa453c33d1626fa211f5422613
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-08-07 18:11:52 -0300

    render posts

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    <h1>Welcome in my Blog</h1>
+    <main>
+      <h2>Latest thoughts</h2>
+      <ul>
+        <li v-for="(post) in posts" :key="post.id">
+          <img :src="post.feature_image" :alt="post.title" />
+            <div class="content">
+              <span v-text="post.authors[0].name" />
+              <p v-text="post.excerpt" />
+              <NuxtLink :to=" {path: post.slug}" v-text="post.title" />
+            </div>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
+import { getPosts } from "@/api/posts"
   export default {
-    
+    async asyncData() {
+      const posts = await getPosts();
+      return  { posts: posts}
+    }
   }
 </script>
 
